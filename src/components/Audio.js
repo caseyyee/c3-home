7415224e25951e7330f85be5cb304c2e9c241031
Casey Yee <caseyyee.ca@gmail.com>
2020-04-01 00:25:54 -0700

    Multi source select

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -1,29 +1,69 @@
 import React, { useState } from "react";
-import {
-  Heading,
-  Box,
-  Checkbox,
-  MenuList,
-  MenuItem,
-  Flex,
-  Menu,
-  Button,
-  MenuButton,
-  Text
-} from "@chakra-ui/core";
+import { Heading, Box, Checkbox, Flex, Text, Select } from "@chakra-ui/core";
+
+const sources = ["Source 1", "Source 2", "Source 3"];
+
+const initialState = [
+  {
+    label: "Room 1",
+    source: 0,
+    checked: false,
+    value: parseInt(Math.random() * 10)
+  },
+  {
+    label: "Room 2",
+    source: 1,
+    checked: false,
+    value: parseInt(Math.random() * 10)
+  },
+  {
+    label: "Room 3",
+    source: 2,
+    checked: false,
+    value: parseInt(Math.random() * 10)
+  },
+  {
+    label: "Room 4",
+    source: 0,
+    checked: false,
+    value: parseInt(Math.random() * 10)
+  },
+  {
+    label: "Room 5",
+    source: 0,
+    checked: false,
+    value: parseInt(Math.random() * 10)
+  }
+];
 
 export default () => {
-  const [rooms, setRooms] = useState([
-    { label: "Room 1", checked: false },
-    { label: "Room 2", checked: false },
-    { label: "Room 3", checked: false },
-    { label: "Room 4", checked: false }
-  ]);
+  const [rooms, setRooms] = useState(initialState);
 
   const checkChanged = index => {
     setRooms(prevState => {
       prevState[index].checked = !prevState[index].checked;
       return [...prevState];
+    });
+  };
+
+  const onSourceChange = (index, selectedIndex) => {
+    setRooms(prevState => {
+      prevState[index].source = selectedIndex;
+      return [...prevState];
+    });
+  };
+
+  const onMultiSourceChange = e => {
+    const newSourceId = e.target.selectedIndex;
+
+    setRooms(prevState => {
+      const newState = prevState.map(room => {
+        if (room.checked) {
+          room.source = newSourceId;
+        }
+        return { ...room };
+      });
+      return [...newState];
     });
   };
 
@@ -41,7 +81,6 @@ export default () => {
             p="1rem"
           >
             <Checkbox
-              value="room-1"
               checked={rooms.checked}
               onChange={() => checkChanged(index)}
             />
@@ -49,34 +88,42 @@ export default () => {
               <Text width="10rem" p="10px" mx="2rem">
                 {room.label}
               </Text>
-              <Menu mx="2rem">
-                <MenuButton as={Button} rightIcon="chevron-down">
-                  Select Source
-                </MenuButton>
-                <MenuList>
-                  <MenuItem>Source 1</MenuItem>
-                  <MenuItem>Source 2</MenuItem>
-                  <MenuItem>Source 3</MenuItem>
-                </MenuList>
-                <Flex>
-                  <Box mx="2rem">
-                    <ch5-button label="Mute" sendEventOnClick="22" />
-                  </Box>
-                  <Box mr="1rem">
-                    <ch5-button label="Off" sendEventOnClick="22" />
-                  </Box>
-                  <Box mr="1rem">
-                    <ch5-slider
-                      handleShape="circle"
-                      sendEventOnChange="24"
-                      receiveStateValue="23"
-                    />
-                  </Box>
-                  <Box>
-                    <ch5-button label="On" sendEventOnClick="21" />
-                  </Box>
-                </Flex>
-              </Menu>
+              <Box mx="2rem">
+                <Select
+                  placeholder="Source"
+                  onChange={e => onSourceChange(index, e.target.selectedIndex)}
+                  value={room.source - 1}
+                >
+                  {sources.map((source, index) => {
+                    return (
+                      <option key={`source-${index}`} value={index}>
+                        {source}
+                      </option>
+                    );
+                  })}
+                </Select>
+              </Box>
+              <Flex>
+                <Box mx="2rem">
+                  <ch5-button label="Mute" sendEventOnClick="22" />
+                </Box>
+                <Box mr="1rem">
+                  <ch5-button label="Off" sendEventOnClick="22" />
+                </Box>
+                <Box mr="1rem">
+                  <ch5-slider
+                    handleShape="circle"
+                    sendEventOnChange="24"
+                    receiveStateValue="23"
+                    value={room.value}
+                    min="0"
+                    max="10"
+                  />
+                </Box>
+                <Box>
+                  <ch5-button label="On" sendEventOnClick="21" />
+                </Box>
+              </Flex>
             </Flex>
           </Flex>
         ))}
@@ -84,7 +131,7 @@ export default () => {
         {checkedCount > 0 && (
           <Flex
             style={{ borderBottom: "1px solid lightgray" }}
-            background="lightgray"
+            background="#efefef"
             mt="4rem"
             p="1rem"
           >
@@ -92,34 +139,36 @@ export default () => {
               <Text p="10px" mx="2rem" width="11rem">
                 {checkedCount} rooms selected.
               </Text>
-              <Menu mx="2rem">
-                <MenuButton as={Button} rightIcon="chevron-down">
-                  Select Source
-                </MenuButton>
-                <MenuList>
-                  <MenuItem>Source 1</MenuItem>
-                  <MenuItem>Source 2</MenuItem>
-                  <MenuItem>Source 3</MenuItem>
-                </MenuList>
-                <Flex>
-                  <Box mx="2rem">
-                    <ch5-button label="Mute" sendEventOnClick="22" />
-                  </Box>
-                  <Box mr="1rem">
-                    <ch5-button label="Off" sendEventOnClick="22" />
-                  </Box>
-                  <Box mr="1rem">
-                    <ch5-slider
-                      handleShape="circle"
-                      sendEventOnChange="24"
-                      receiveStateValue="23"
-                    />
-                  </Box>
-                  <Box>
-                    <ch5-button label="On" sendEventOnClick="21" />
-                  </Box>
-                </Flex>
-              </Menu>
+
+              <Box>
+                <Select placeholder="Source" onChange={onMultiSourceChange}>
+                  {sources.map((source, index) => {
+                    return (
+                      <option key={`source-${index}`} value={index}>
+                        {source}
+                      </option>
+                    );
+                  })}
+                </Select>
+              </Box>
+              <Flex>
+                <Box mx="2rem">
+                  <ch5-button label="Mute" sendEventOnClick="22" />
+                </Box>
+                <Box mr="1rem">
+                  <ch5-button label="Off" sendEventOnClick="22" />
+                </Box>
+                <Box mr="1rem">
+                  <ch5-slider
+                    handleShape="circle"
+                    sendEventOnChange="24"
+                    receiveStateValue="23"
+                  />
+                </Box>
+                <Box>
+                  <ch5-button label="On" sendEventOnClick="21" />
+                </Box>
+              </Flex>
             </Flex>
           </Flex>
         )}
